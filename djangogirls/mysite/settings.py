948869dff09b719a693e8c039a9d0336ffdd0856
Abegail Sieffers - Isidro <chabbyabe@gmail.com>
2026-03-27 23:01:24 +0100

    Update settings.py Change timezone, allowed_host and add static root

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+ug4@l%4+t&nuui8s88%@jf@u_r-2q(ewlq2pjw68_is9-&w*c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Amsterdam'
 
 USE_I18N = True
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
